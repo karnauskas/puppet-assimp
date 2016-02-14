88eaef11b9270a724eb790953afe51b46fca1900
Marius Karnauskas <marius.karnauskas@gmail.com>
2016-02-14 05:10:22 +0000

    Adding Travis

---
+++ manifests/init.pp
@@ -0,0 +1,20 @@
+# == Class: assimp
+#
+# Install assimp library and it's Python bindings
+# 
+# === Examples
+#
+#  class { 'assimp':
+#  }
+#
+# === Authors
+#
+# Marius Karnauskas <marius.karnauskas@gmail.com>
+#
+# === Copyright
+#
+# Copyright 2016 Marius Karnauskas
+#
+class assimp {
+
+}
